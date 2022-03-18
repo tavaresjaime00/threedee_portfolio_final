ffc96dda48157c028c20113438dce0da191b6240
Jaime Tavares <tavaresjaime00@gmail.com>
2022-03-18 01:07:58 -0400

    commin on 'main'

--- components/voxel-donut.js
+++ components/voxel-donut.js
@@ -1,59 +1,59 @@
-import { useState, useEffect, useRef, useCallback } from 'react'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { loadGLTFModel } from '../libs/model'
-import { DonutSpinner, DonutContainer } from './voxel-donut-loader'
+import { useState, useEffect, useRef, useCallback } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { loadGLTFModel } from "../libs/model";
+import { DonutSpinner, DonutContainer } from "./voxel-donut-loader";
 
 function easeOutCirc(x) {
-  return Math.sqrt(1 - Math.pow(x - 1, 4))
+  return Math.sqrt(1 - Math.pow(x - 1, 4));
 }
 
 const VoxelDonut = () => {
-  const refContainer = useRef()
-  const [loading, setLoading] = useState(true)
-  const [renderer, setRenderer] = useState()
-  const [_camera, setCamera] = useState()
-  const [target] = useState(new THREE.Vector3(-0.5, 1.2, 0))
+  const refContainer = useRef();
+  const [loading, setLoading] = useState(true);
+  const [renderer, setRenderer] = useState();
+  const [_camera, setCamera] = useState();
+  const [target] = useState(new THREE.Vector3(-0.5, 1.2, 0));
   const [initialCameraPosition] = useState(
     new THREE.Vector3(
       20 * Math.sin(0.2 * Math.PI),
       10,
       20 * Math.cos(0.2 * Math.PI)
     )
-  )
-  const [scene] = useState(new THREE.Scene())
-  const [_controls, setControls] = useState()
+  );
+  const [scene] = useState(new THREE.Scene());
+  const [_controls, setControls] = useState();
 
   const handleWindowResize = useCallback(() => {
-    const { current: container } = refContainer
+    const { current: container } = refContainer;
     if (container && renderer) {
-      const scW = container.clientWidth
-      const scH = container.clientHeight
+      const scW = container.clientWidth;
+      const scH = container.clientHeight;
 
-      renderer.setSize(scW, scH)
+      renderer.setSize(scW, scH);
     }
-  }, [renderer])
+  }, [renderer]);
 
   /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
-    const { current: container } = refContainer
+    const { current: container } = refContainer;
     if (container && !renderer) {
-      const scW = container.clientWidth
-      const scH = container.clientHeight
+      const scW = container.clientWidth;
+      const scH = container.clientHeight;
 
       const renderer = new THREE.WebGLRenderer({
         antialias: true,
-        alpha: true
-      })
-      renderer.setPixelRatio(window.devicePixelRatio)
-      renderer.setSize(scW, scH)
-      renderer.outputEncoding = THREE.sRGBEncoding
-      container.appendChild(renderer.domElement)
-      setRenderer(renderer)
+        alpha: true,
+      });
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(scW, scH);
+      renderer.outputEncoding = THREE.sRGBEncoding;
+      container.appendChild(renderer.domElement);
+      setRenderer(renderer);
 
       // 640 -> 240
       // 8   -> 6
-      const scale = scH * 0.005 + 4.8
+      const scale = scH * 0.005 + 4.8;
       const camera = new THREE.OrthographicCamera(
         -scale,
         scale,
@@ -61,69 +61,71 @@ const VoxelDonut = () => {
         -scale,
         0.01,
         50000
-      )
-      camera.position.copy(initialCameraPosition)
-      camera.lookAt(target)
-      setCamera(camera)
+      );
+      camera.position.copy(initialCameraPosition);
+      camera.lookAt(target);
+      setCamera(camera);
 
-      const ambientLight = new THREE.AmbientLight(0xaaaaaa, 1)
-      scene.add(ambientLight)
+      const ambientLight = new THREE.AmbientLight(0xaaaaaa, 1);
+      scene.add(ambientLight);
 
-      const controls = new OrbitControls(camera, renderer.domElement)
-      controls.autoRotate = true
-      controls.target = target
-      setControls(controls)
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.autoRotate = true;
+      controls.target = target;
+      setControls(controls);
 
-      loadGLTFModel(scene, 'images/3d/Dino.glb', {
+      loadGLTFModel(scene, "images/3d/Dino.glb", {
         receiveShadow: false,
-        castShadow: false
+        castShadow: false,
       }).then(() => {
-        animate()
-        setLoading(false)
-      })
+        animate();
+        setLoading(false);
+      });
 
-      let req = null
-      let frame = 0
+      let req = null;
+      let frame = 0;
       const animate = () => {
-        req = requestAnimationFrame(animate)
+        req = requestAnimationFrame(animate);
 
-        frame = frame <= 100 ? frame + 1 : frame
+        frame = frame <= 100 ? frame + 1 : frame;
 
         if (frame <= 100) {
-          const p = initialCameraPosition
-          const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20
+          const p = initialCameraPosition;
+          const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20;
 
-          camera.position.y = 10
+          camera.position.y = 10;
           camera.position.x =
-            p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed)
+            p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed);
           camera.position.z =
-            p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed)
-          camera.lookAt(target)
+            p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed);
+          camera.lookAt(target);
         } else {
-          controls.update()
+          controls.update();
         }
 
-        renderer.render(scene, camera)
-      }
+        renderer.render(scene, camera);
+      };
 
       return () => {
-        console.log('unmount')
-        cancelAnimationFrame(req)
-        renderer.dispose()
-      }
+        console.log("unmount");
+        cancelAnimationFrame(req);
+        renderer.dispose();
+      };
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    window.addEventListener('resize', handleWindowResize, false)
+    window.addEventListener("resize", handleWindowResize, false);
     return () => {
-      window.removeEventListener('resize', handleWindowResize, false)
-    }
-  }, [renderer, handleWindowResize])
+      window.removeEventListener("resize", handleWindowResize, false);
+    };
+  }, [renderer, handleWindowResize]);
 
   return (
-    <DonutContainer ref={refContainer}>{loading && <DonutSpinner />}</DonutContainer>
-  )
-}
+    <DonutContainer ref={refContainer}>
+      {loading && <DonutSpinner />}
+    </DonutContainer>
+  );
+};
 
-export default VoxelDonut
+export default VoxelDonut;
