e31d5e33f640c9337f294ef92f38b590d07e39de
Jaime Tavares <tavaresjaime00@gmail.com>
2023-05-04 15:45:11 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -49,18 +49,12 @@ const Page = () => {
 
           <Box
             flexShrink={0}
-            mt={{ base: 4, md: 0 }}
-            ml={{ md: 6 }}
             align="center"
           >
             <Box
-              borderColor="whiteAlpha.800"
-              borderWidth={2}
-              borderStyle="solid"
               w="100px"
               h="100px"
               display="inline-block"
-              borderRadius="full"
               overflow="hidden"
             >
               <Image
