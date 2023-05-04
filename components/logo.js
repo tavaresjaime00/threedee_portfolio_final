ff331bd3f6672459ae3caac3a8b02c71306fcbf8
Jaime Tavares <tavaresjaime00@gmail.com>
2023-05-04 15:08:37 -0400

    changed logo to newly designed one

--- components/logo.js
+++ components/logo.js
@@ -20,7 +20,7 @@ const LogoBox = styled.span`
 `
 
 const Logo = () => {
-    const footPrintImg = '/images/clo.png'
+    const footPrintImg = '/images/logo_noback.png'
 
     return (
         <Link href="/" passHref>
